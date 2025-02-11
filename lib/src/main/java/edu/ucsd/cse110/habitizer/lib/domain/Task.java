7e965fa87525a8965d422e4ec9a96b7581b85f78
UCSDNobel1998 <xiw170@ucsd.edu>
2025-02-10 21:23:03 -0800

    Add a Timer class and subclasses of Timer class

--- lib/src/main/java/edu/ucsd/cse110/habitizer/lib/domain/Task.java
+++ lib/src/main/java/edu/ucsd/cse110/habitizer/lib/domain/Task.java
@@ -1,57 +1,30 @@
 package edu.ucsd.cse110.habitizer.lib.domain;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import edu.ucsd.cse110.habitizer.lib.timer.TaskTimer;
 
-import java.io.Serializable;
-import java.time.LocalDateTime;
-
-import edu.ucsd.cse110.habitizer.lib.domain.timer.TaskTimer;
-
-public class Task implements Serializable {
-    private  @Nullable Integer id;
-    private  String taskName;
+public class Task {
+    private final String taskName;
+    private final TaskTimer taskTimer = new TaskTimer();
     private boolean isCompleted = false;
 
-    private boolean isCheckedOff;
-    private int duration = 0;
-
-    public Task(@Nullable Integer id, String taskName, boolean isCheckedOff) {
-        this.id = id;
-        this.taskName = taskName;
-        this.isCheckedOff = isCheckedOff;
-    }
-
-    public void setTaskId(int id) {
-        this.id = id;
-    }
-
-    public void setTaskName(String taskName) {
+    public Task(String taskName) {
         this.taskName = taskName;
     }
 
-    public Task withId(int id) {
-        return new Task(id, this.taskName, isCheckedOff);
+    // Start the task
+    public void startTask() {
+        taskTimer.start();
     }
 
-
-    public int getDuration() {
-        return duration;
+    // End the task
+    public void completeTask() {
+        taskTimer.end();
+        isCompleted = true;
     }
 
-    public void setDurationAndComplete(int duration) {
-        this.duration = duration;
-        this.isCompleted = true;
-    }
-
-    // Returns if task is checked off
-    public boolean isCheckedOff() {
-        return isCheckedOff;
-    }
-
-    // Sets the Check
-    public void setCheckedOff(boolean checkedOff) {
-        this.isCheckedOff = checkedOff;
+    // Get the time for the task (round down)
+    public long getDurationMinutes() {
+        return taskTimer.getElapsedMinutes();
     }
 
     // Getters
@@ -59,15 +32,7 @@ public class Task implements Serializable {
         return taskName;
     }
 
-    @Override
-    public String toString() {
-        return taskName;
-    }
-
-    public Integer getTaskId() { return id; }
-
     public boolean isCompleted() {
         return isCompleted;
     }
-
 }
