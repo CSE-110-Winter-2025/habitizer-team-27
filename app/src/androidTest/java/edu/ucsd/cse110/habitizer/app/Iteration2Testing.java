a55dfb7f4ee186d66e9ee402a34d31bb7d8e3e1d
Paris Aguilar-ulloa <parisaguilar-ulloa@Pariss-MacBook-Air.local>
2025-02-15 23:57:10 -0800

    Add Iteration 2 tests and update

--- app/src/androidTest/java/edu/ucsd/cse110/habitizer/app/Iteration2Testing.java
+++ app/src/androidTest/java/edu/ucsd/cse110/habitizer/app/Iteration2Testing.java
@@ -25,46 +25,47 @@ public class Iteration2Testing {
             new ActivityScenarioRule<>(MainActivity.class);
 
     /**
-     * test 1: end routine
-     *  start a routine by tapping the "start routine" button in the home screen
-     *  tap "end routine" and verify the routine completes
+     * test: end routine flow
+     *  starts a routine
+     *  taps "end routine" and verifies that the routine ends
+     *  the expected text is "routine ended"
      */
     @Test
     public void testEndRoutineFlow() {
-        //home screen has a ListView (@+id/card_list) of routines using routine_list.xml
-        //if there's only one routine, we can simply click the "start routine" button:
+        //home screen has a ListView (@+id/card_list) of routines using routine_list.xml.
+        //since there are both Morning and Evening routines the test uses (Morning) routine.
         onView(withId(R.id.start_routine_button)).perform(click());
 
-        //now we're in fragment_routine_screen.xml which has the "End Routine" button.
+        //now in fragment_routine_screen.xml tap the "End Routine" button.
         onView(withId(R.id.end_routine_button)).perform(click());
 
-        //sfter ending we expect the button text might change to "routine complete!"
+        //after ending we expect the button text to change to "routine ended".
         onView(withId(R.id.end_routine_button))
-                .check(matches(withText("Routine Complete!")));
+                .check(matches(withText("Routine Ended")));
     }
 
     /**
-     * test 2: add a task to a routine
-     *  after starting a routine, tap "Add Task" to open a dialog
-     *  type in a new task name and confirm
-     *  verify the new task appears in the routine_list
+     * test: add a task to a routine
+     *  after starting a routine tap "add task" to open the dialog
+     *  type a new task name and confirm by tapping the "OK" button.
+     *  verify the new task appears in the routine_list.
      */
     @Test
     public void testAddTaskFlow() {
         //start the routine from the home screen
         onView(withId(R.id.start_routine_button)).perform(click());
 
-        //in fragment_routine_screen.xml, there's a "Add Task" button: @+id/add_task_button
+        //In fragment_routine_screen.xml tap the "add task" button.
         onView(withId(R.id.add_task_button)).perform(click());
 
-        //this shows dialog_add_tasks.xml which has @+id/task_name_edit_text
+        //the dialog (dialog_add_tasks.xml) appears enter the task name.
         onView(withId(R.id.task_name_edit_text))
                 .perform(replaceText("Brush Teeth"), closeSoftKeyboard());
 
-        //we assume there's a "save" button in the dialog with text "save"
-        onView(withText("Save")).perform(click());
+        //tap the "OK" button
+        onView(withText("OK")).perform(click());
 
-        //now the new task "brush teeth" should appear in the routine_list ListView.
+        //verify the new task "Brush Teeth" appears in the routine_list ListView.
         onData(anything())
                 .inAdapterView(withId(R.id.routine_list))
                 .atPosition(0)
@@ -73,43 +74,39 @@ public class Iteration2Testing {
     }
 
     /**
-     * test 3: adding a task with an empty name
-     *   start a routine
-     *   tap "Add Task"
-     *   leave the name blank and tap "Save"
+     * test: adding a task with an empty name
+     *  start a routine tap "add task" leave the name blank and tap "OK".
      */
     @Test
     public void testAddTaskEmptyName() {
         //start routine
         onView(withId(R.id.start_routine_button)).perform(click());
 
-        //click "add task"
+        //tap "Add Task"
         onView(withId(R.id.add_task_button)).perform(click());
 
-        //don't type anything (empty name) just tap "save"
-        onView(withText("Save")).perform(click());
+        //do not enter any text and tap the "OK" button.
+        onView(withText("OK")).perform(click());
     }
 
     /**
-     * test 4: verify timer text updates
-     *   start a routine
-     *   mark 1 or more tasks as complete
-     *   check that the "actual_time" TextView in fragment_routine_screen changes from "0" to something else
+     * test: verify timer text updates after completing a task
+     *  start a routine and verify the "actual_time" TextView initially displays "0".
+     *  complete a task at position 0 by tapping
      */
     @Test
     public void testTimeUpdatesAfterCompletion() {
         //start routine
         onView(withId(R.id.start_routine_button)).perform(click());
 
-        //the routine screen shows "actual_time" (TextView) default "0".
+        //verify the "actual_time" TextView initially displays "0".
         onView(withId(R.id.actual_time)).check(matches(withText("0")));
 
-        //complete a task at position 0
+        //complete a task at position 0 by tapping its check box
         onData(anything())
                 .inAdapterView(withId(R.id.routine_list))
                 .atPosition(0)
                 .onChildView(withId(R.id.check_task))
                 .perform(click());
     }
-
 }
