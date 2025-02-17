f76548a3910b79da3507840598bd5217fed83ab6
UCSDNobel1998 <xiw170@ucsd.edu>
2025-02-16 21:15:12 -0800

    Modify the test to allign with the app

--- app/src/androidTest/java/edu/ucsd/cse110/habitizer/app/Milestone1Testing.java
+++ app/src/androidTest/java/edu/ucsd/cse110/habitizer/app/Milestone1Testing.java
@@ -1,5 +1,6 @@
 package edu.ucsd.cse110.habitizer.app;
 
+import androidx.test.espresso.Espresso;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
@@ -11,8 +12,10 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasSibling;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.Matchers.anything;
 
 @RunWith(AndroidJUnit4.class)
@@ -31,12 +34,14 @@ public class Milestone1Testing {
     public void testStartRoutine() {
         //on the home screen there is a ListView of routines (routine_list.xml)
         //with a "start routine" button (R.id.start_routine_button)
-        onView(withId(R.id.start_routine_button)).perform(click());
+        Espresso.onView(allOf(
+                withId(R.id.start_routine_button), hasSibling(withText("Morning"))
+        )).perform(click());
 
         //verify that the routine screen is shown
         //in fragment_routine_screen.xm  there's a TextView with id routine_name_task.
         onView(withId(R.id.routine_name_task))
-                .check(matches(withText("Routine Name")));
+                .check(matches(withText("Morning")));
     }
 
     /**
@@ -48,7 +53,9 @@ public class Milestone1Testing {
     @Test
     public void testMarkStepComplete() {
         //start the routine.
-        onView(withId(R.id.start_routine_button)).perform(click());
+        Espresso.onView(allOf(
+                withId(R.id.start_routine_button), hasSibling(withText("Morning"))
+        )).perform(click());
 
         //assume the routine screen contains a ListView
         //the first task (position 0) is "shower" with a checkBox
@@ -59,12 +66,11 @@ public class Milestone1Testing {
                 .onChildView(withId(R.id.check_task))
                 .perform(click());
 
-        //after marking complete assume the task's time is updated from its default "[ - ]" to a duration "19m").
-        //adjust "19m" if your implementation displays a different value
+        //after marking complete assume the task's time is updated from its default "" to a duration "1m").
         onData(anything())
                 .inAdapterView(withId(R.id.routine_list))
                 .atPosition(0)
                 .onChildView(withId(R.id.task_time))
-                .check(matches(withText("19m")));
+                .check(matches(withText("1m")));
     }
 }
